6653e518c8f353ffbaf6317a3bf0d2a50d949c36
Mpendulo <mpendu2016@gmail.com>
2025-12-01 23:56:46 +0200

    MajorChordFormulaButton pendulo

---
+++ SideBarDropDownButton.js
@@ -0,0 +1,27 @@
+const MajorDropDownButton1 = document.getElementById("MajorChordFormation1");
+const ChordFormImage = document.getElementById("MajorChordImage");
+
+ChordFormImage.style.display ="none";
+
+MajorDropDownButton1.addEventListener("click", (event) => {
+    if(ChordFormImage.style.display == "none"){
+        ChordFormImage.style.display ="block";
+    } else {
+        ChordFormImage.style.display ="none";
+    }
+});
+
+const MinorDropDownButton1 = document.getElementById("MajorChordFormation1");
+const ChordFormImage = document.getElementById("MajorChordImage");
+
+ChordFormImage.style.display ="none";
+
+MajorDropDownButton1.addEventListener("click", (event) => {
+    if(ChordFormImage.style.display == "none"){
+        ChordFormImage.style.display ="block";
+    } else {
+        ChordFormImage.style.display ="none";
+    }
+});
+
+
